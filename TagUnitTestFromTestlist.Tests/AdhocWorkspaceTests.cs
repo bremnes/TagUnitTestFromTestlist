ce21e29e17ebe030feec7d103f01e9ccfd122f76
Lars Kristian Bremnes <bremnes@gmail.com>
2015-07-14 22:57:44 +0200

    Structure change, moved everything one level up

---
+++ TagUnitTestFromTestlist.Tests/AdhocWorkspaceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Linq;
+
+namespace TagUnitTestFromTestlist.Tests
+{
+    [TestClass]
+    public class AdhocWorkspaceTests
+    {
+        [TestMethod]
+        public void ManualTestToCheckLogic()
+        {
+            var adhocWorkspace = new AdhocWorkspace();
+            var solution = adhocWorkspace.CurrentSolution;
+
+            var newProject = adhocWorkspace.AddProject("Project.Test", LanguageNames.CSharp);
+            adhocWorkspace.AddDocument(newProject.Id, "TestFile.cs", SourceText.From("public class Bar { }"));
+
+            Assert.AreEqual(1, adhocWorkspace.CurrentSolution.Projects.Count());
+            var project = adhocWorkspace.CurrentSolution.Projects.Single();
+            Assert.AreEqual("Project.Test", project.Name);
+            Assert.AreEqual(1, project.Documents.Count());
+            Assert.AreEqual("TestFile.cs", project.Documents.Single().Name);
+        }
+    }
+}
---
+++ TagUnitTestFromTestlist.Utils/VsmdiParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using TagUnitTestFromTestlist.Models;
+
+namespace TagUnitTestFromTestlist.Utils
+{
+    public class VsmdiParser
+    {
+        public List<TestList> ReadFile(string filepath)
+        {
+            var vsmdiXmlDocument = XDocument.Load(filepath);
+
+            var testlists = vsmdiXmlDocument
+                                .Descendants()
+                                .Where(d => d.Name.LocalName.Equals("TestList"))
+                                .Select(tl => new TestList {
+                                    Name = tl.Attribute("name").Value,
+                                    Tests = tl.Descendants()
+                                                .Where(d => d.Name.LocalName.Equals("TestLink"))
+                                                .Select(t => new Test
+                                                {
+                                                    Name = tl.Attribute("name").Value
+                                                })
+                                                .ToList()
+                                })
+                                .ToList();
+
+            return testlists;
+        }
+    }
+}
