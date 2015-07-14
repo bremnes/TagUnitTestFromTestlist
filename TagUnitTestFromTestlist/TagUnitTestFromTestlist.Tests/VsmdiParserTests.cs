e75059b019b9c915f2f46c4e8cb9bf121eb6ffd8
Lars Kristian Bremnes <bremnes@gmail.com>
2015-07-14 20:16:35 +0200

    Reads a vsmdi-file from disk

---
+++ TagUnitTestFromTestlist/TagUnitTestFromTestlist.Tests/VsmdiParserTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TagUnitTestFromTestlist.Utils;
+
+namespace TagUnitTestFromTestlist.Tests
+{
+    [TestClass]
+    public class VsmdiParserTests
+    {
+        VsmdiParser _vsmdiParser;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _vsmdiParser = new VsmdiParser();
+        }
+
+        [TestMethod]
+        public void CanReadFileFromDisk()
+        {
+            _vsmdiParser.ReadFile("testlist.vsmdi");
+        }
+    }
+}
---
+++ TagUnitTestFromTestlist/TagUnitTestFromTestlist.Utils/VsmdiParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace TagUnitTestFromTestlist.Utils
+{
+    public class VsmdiParser
+    {
+        public void ReadFile(string filepath)
+        {
+            var vsmdiXmlDocument = XDocument.Load(filepath);
+        }
+    }
+}
