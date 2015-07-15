ef764f51d9f12e9ef8c20bff807adea91cbc1f9c
Lars Kristian Bremnes <bremnes@gmail.com>
2015-07-15 22:38:17 +0200

    Takes an incoming Document and returns a SyntaxTree with correct tagged methods - those who are TestMethods and in the given list will be tagget with TestCategory

--- TagUnitTestFromTestlist.Categorizer/DocumentManipulator.cs
+++ TagUnitTestFromTestlist.Categorizer/DocumentManipulator.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +13,113 @@ namespace TagUnitTestFromTestlist.Categorizer
     public class DocumentManipulator
     {
         Document _document;
+        Workspace _workspace;
 
-        public DocumentManipulator(Document document)
+        public DocumentManipulator(Document document, Workspace workspace)
         {
+            _workspace = workspace;
             _document = document;
         }
 
-        public Document CategorizeTestMethods()
+        public SyntaxTree CategorizeTestMethods(List<string> testmethods, string category)
         {
             var existingSyntaxNode = GetSyntaxNodeFromDocument().Result;
-            var methods = existingSyntaxNode.DescendantNodes().OfType<MethodDeclarationSyntax>();
+            SyntaxTree newSyntaxTree = SyntaxFactory.SyntaxTree(existingSyntaxNode);
 
-            return _document;
+            var originalMethods = existingSyntaxNode
+                                    .DescendantNodes()
+                                    .OfType<MethodDeclarationSyntax>()
+                                    .Where(m => 
+                                        IsTestMethod(m) 
+                                        && testmethods.Contains(m.Identifier.Value));
+
+            foreach (var originalMethod in originalMethods)
+            {
+                var methodFromNewSyntaxTree = GetMethodFromSyntaxRoot(newSyntaxTree.GetCompilationUnitRoot(), originalMethod);
+                var testCategoryAttribute = FindAttributeWithName(methodFromNewSyntaxTree, "TestCategory");
+
+                if (testCategoryAttribute != null)
+                {
+                    var isAlreadyTaggetWithCategory = testCategoryAttribute.ArgumentList.Arguments.Any(a => a.ToString().Equals(category));
+                    if (isAlreadyTaggetWithCategory)
+                    {
+                        continue;
+                    }
+                }
+
+                var newMethod = AddMethodProperty(methodFromNewSyntaxTree, "TestCategory", category);
+                var newSyntaxNode = newSyntaxTree.GetRoot().ReplaceNode(methodFromNewSyntaxTree, newMethod);
+
+                newSyntaxTree = SyntaxFactory
+                                    .SyntaxTree(Formatter.Format(newSyntaxNode, _workspace))
+                                    .WithFilePath(methodFromNewSyntaxTree.SyntaxTree.FilePath);
+            }
+
+            return newSyntaxTree;
+        }
+        
+        private bool IsTestMethod(MethodDeclarationSyntax method)
+        {
+            var isTestMethod = method.AttributeLists
+                                .Any(al => al.Attributes
+                                            .Any(a => 
+                                                a.Name is IdentifierNameSyntax 
+                                                && ((IdentifierNameSyntax)a.Name).Identifier.Value.Equals("TestMethod")));
+
+            return isTestMethod;
+        }
+
+        private AttributeSyntax FindAttributeWithName(MethodDeclarationSyntax method, string name)
+        {
+            var attributes = method.AttributeLists.SelectMany(al => al.Attributes).ToList();
+            return attributes.SingleOrDefault(a => a.Name is IdentifierNameSyntax && ((IdentifierNameSyntax)a.Name).Identifier.Value.Equals(name));
         }
 
         private async Task<SyntaxNode> GetSyntaxNodeFromDocument()
         {
             var root = await _document.GetSyntaxRootAsync().ConfigureAwait(false);
             return root;
+        }
+
+        static MethodDeclarationSyntax AddMethodProperty(MethodDeclarationSyntax method, string propertyName, string argumentName)
+        {
+            return method.AddAttributeLists(
+                    SyntaxFactory.AttributeList(
+                        SyntaxFactory.SingletonSeparatedList(
+                            SyntaxFactory.Attribute(
+                                SyntaxFactory.IdentifierName(propertyName),
+                                SyntaxFactory.AttributeArgumentList(
+                                    SyntaxFactory.SingletonSeparatedList(
+                                        SyntaxFactory.AttributeArgument(
+                                            SyntaxFactory.LiteralExpression(
+                                                SyntaxKind.StringLiteralExpression,
+                                                SyntaxFactory.Token(
+                                                    default(SyntaxTriviaList),
+                                                    SyntaxKind.StringLiteralToken,
+                                                    argumentName,
+                                                    argumentName,
+                                                    default(SyntaxTriviaList))
+                                                ))))))));
+        }
+
+        static MethodDeclarationSyntax GetMethodFromSyntaxRoot(CompilationUnitSyntax root, MethodDeclarationSyntax method)
+        {
+            try
+            {
+                var methodsInSyntaxRoot = root
+                                            .DescendantNodes()
+                                            .OfType<ClassDeclarationSyntax>()
+                                            .Select(b => b.Members.OfType<MethodDeclarationSyntax>()
+                                                .SingleOrDefault(m => m.Identifier.ValueText == method.Identifier.ValueText 
+                                                                    && m.ParameterList.ToString() == method.ParameterList.ToString()))
+                                            .Where(m => m != null);
+
+                return methodsInSyntaxRoot.Single();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
--- TagUnitTestFromTestlist.Tests/Categorizer/DocumentManipulatorTests.cs
+++ TagUnitTestFromTestlist.Tests/Categorizer/DocumentManipulatorTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TagUnitTestFromTestlist.Categorizer;
-using TagUnitTestFromTestlist.Tests.Builders;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+using System.Linq;
 
 namespace TagUnitTestFromTestlist.Tests.Categorizer
 {
@@ -10,16 +10,47 @@ namespace TagUnitTestFromTestlist.Tests.Categorizer
     public class DocumentManipulatorTests
     {
         [TestMethod]
-        public void CodeFileWithoutTestMethods_DoNotChange()
+        public void MethodNotTestMethod_DoNotChange()
         {
-            var originalCode = "public class Bar { public void test() {} }";
-            var documentManipulator = new DocumentManipulator(DocumentBuilder.Build("test.cs", originalCode));
-            var newDocument = documentManipulator.CategorizeTestMethods();
-
-            SourceText newCode;
-            newDocument.TryGetText(out newCode);
+            var originalCode = "public class Bar { public void MyFancyMethod() {} }";
+            var newCode = CategorizeTestMethods(originalCode, "\"Unit test\"");
 
             Assert.AreEqual(originalCode, newCode.ToString());
+        }
+
+        [TestMethod]
+        public void TestMethodNotInList_NotCategorized()
+        {
+            var originalCode = "public class Bar {[TestMethod] public void MyFancyTest() {}}";
+            var newCode = CategorizeTestMethods(originalCode, "\"Unit test\"", "MethodToTag");
+
+            Assert.AreEqual(originalCode, newCode.ToString());
+        }
+
+        [TestMethod]
+        public void TestMethodInList_Categorized()
+        {
+            var originalCode = "public class Bar {[TestMethod] public void MyFancyTest() {}}";
+            var newCode = CategorizeTestMethods(originalCode, "\"Unit test\"", "MyFancyTest");
+
+            Assert.AreNotEqual(originalCode, newCode);
+            Assert.IsTrue(newCode.Contains("[TestCategory(\"Unit test\")]"));
+        }
+
+        private string CategorizeTestMethods(string originalCode, string testCategoryToAdd, params string[] testToTag)
+        {
+            Document document;
+            using (var adhocWorkspace = new AdhocWorkspace())
+            {
+                var solution = adhocWorkspace.CurrentSolution;
+                var newProject = adhocWorkspace.AddProject("Project", LanguageNames.CSharp);
+                document = adhocWorkspace.AddDocument(newProject.Id, "TestFile.cs", SourceText.From(originalCode));
+
+                var documentManipulator = new DocumentManipulator(document, adhocWorkspace);
+                var manipulatedSyntaxTree = documentManipulator.CategorizeTestMethods(testToTag.ToList(), testCategoryToAdd);
+                
+                return manipulatedSyntaxTree.ToString();
+            }
         }
     }
 }
